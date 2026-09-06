c5c551831a8406e09ffa45c9e668fd71301de2b8
Lokesh Kaki <lkaki@student.gitam.edu>
2026-09-06 14:56:56 +0530

    Polyalphabetic Substitution Cipher

--- polyalphabetic_cipher.c
+++ polyalphabetic_cipher.c
@@ -1,37 +1,30 @@
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
-
-int isValid(char text[]) {
-    for(int i=0; text[i]; i++) {
-        if(!isalpha(text[i]))
-            return 0;
-    }
-    return 1;
-}
+#include <stdio.h>
+#include <string.h>
 
 int main() {
-    char text[100],key[100];
-    printf("Enter Plain Text: ");
-    fgets(text, sizeof(text), stdin);
-    text[strcspn(text, "\n")] = '\0';
-    printf("Enter Key: ");
-    fgets(key, sizeof(key), stdin);
-    key[strcspn(key, "\n")] = '\0';
-    if(!isValid(text) || !isValid(key)) {
-        printf("Invalid Input");
-        return 0;
+    char text[100], key[100], enc[100], dec[100];
+    int i, j = 0, k;
+    printf("Enter text: ");
+    scanf("%s", text);
+    printf("Enter key: ");
+    scanf("%s", key);
+    k = strlen(key);
+
+    // Encryption
+    for(i = 0; text[i]; i++) {
+        enc[i] = (text[i] - 'A' + key[j] - 'A') % 26 + 'A';
+        j = (j + 1) % k;
     }
-    for(int i=0; text[i]; i++)
-        text[i] = toupper(text[i]);
-    for(int i=0; key[i]; i++)
-        key[i] = toupper(key[i]);
-    int len = strlen(text);
-    int keyLen = strlen(key);
-    printf("Cipher Text: ");
-    for(int i=0; i<len; i++) {
-        char c = ((text[i]-'A') + (key[i%keyLen]-'A'))%26 + 'A';
-        printf("%c", c);
+    enc[i] = '\0';
+    
+    // Decryption
+    j = 0;
+    for(i = 0; enc[i]; i++) {
+        dec[i] = (enc[i] - 'A' - (key[j] - 'A') + 26) % 26 + 'A';
+        j = (j + 1) % k;
     }
+    dec[i] = '\0';
+    printf("Encrypted: %s\n", enc);
+    printf("Decrypted: %s\n", dec);
     return 0;
 }
