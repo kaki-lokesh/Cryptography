f67acd2eb0aeb3d41ae619b2d5712dd8ffab7783
Lokesh Kaki <lkaki@student.gitam.edu>
2026-09-06 17:37:53 +0530

    Substitution: Caesar Cipher

--- caesar_cipher.c
+++ caesar_cipher.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int isValid(char s[]) {
+    for(int i=0; s[i]; i++)
+        if(!isalpha((unsigned char)s[i]))
+            return 0;
+    return 1;
+}
 
 int main() {
     char text[100], enc[100], dec[100];
     int key, i;
     printf("Enter text: ");
-    fgets(text, 100, stdin);
+    scanf("%99s", text);
+    if(!isValid(text)) {
+        printf("Invalid Input");
+        return 0;
+    }
     printf("Enter key: ");
     scanf("%d", &key);
-
-    // Encryption
-    for(i = 0; text[i]; i++) {  
-        if(text[i] >= 'A' && text[i] <= 'Z')
-            enc[i] = (text[i] - 'A' + key) % 26 + 'A';
-        else
-            enc[i] = text[i];
+    key = (key % 26 + 26) % 26;
+    
+    // Encryption & Decryption
+    for(i=0; text[i]; i++) {
+        text[i] = toupper((unsigned char)text[i]);
+        enc[i] = (text[i]-'A'+key)%26 + 'A';
+        dec[i] = (enc[i]-'A'-key+26)%26 + 'A';
     }
-    enc[i] = '\0';
-
-    // Decryption
-    for(i = 0; enc[i]; i++) {
-        if(enc[i] >= 'A' && enc[i] <= 'Z')
-            dec[i] = (enc[i] - 'A' - key + 26) % 26 + 'A';
-        else
-            dec[i] = enc[i];
-    }
-    dec[i] = '\0';
-    printf("Encrypted: %s", enc);
-    printf("Decrypted: %s", dec);
+    enc[i] = dec[i] = '\0';
+    printf("Encrypted: %s\n", enc);
+    printf("Decrypted: %s\n", dec);
     return 0;
 }
