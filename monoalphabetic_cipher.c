bcb324fef29233f30e4215c7051ead56807d41f8
Lokesh Kaki <lkaki@student.gitam.edu>
2026-09-06 17:37:01 +0530

    Substitution: Monoalphabetic Cipher

--- monoalphabetic_cipher.c
+++ monoalphabetic_cipher.c
@@ -2,31 +2,47 @@
 #include <string.h>
 #include <ctype.h>
 
+int isValid(char s[]) {
+    for(int i=0; s[i]; i++)
+        if(!isalpha((unsigned char)s[i]))
+            return 0;
+    return 1;
+}
+
 int main() {
-    char text[100], key[26];
-    int i;
+    char text[100], key[27], enc[100], dec[100];
+    int i, j, used[26] = {0};
     printf("Enter text: ");
-    fgets(text, 100, stdin);
+    scanf("%99s", text);
     printf("Enter key: ");
-    fgets(key, 26, stdin);
+    scanf("%26s", key);
+    if(!isValid(text) || strlen(key) != 26 || !isValid(key)) {
+        printf("Invalid Input");
+        return 0;
+    }
+    for(i=0; i<26; i++) {
+        key[i] = toupper((unsigned char)key[i]);
+        if(used[key[i]-'A']) {
+            printf("Invalid Input");
+            return 0;
+        }
+        used[key[i]-'A'] = 1;
+    }
 
     // Encryption
-    printf("Encrypted: ");
-    for(i = 0; text[i]; i++) {
-        if(isalpha(text[i])) {
-            char c = toupper(text[i]);
-            putchar(key[c - 'A']);
-        } else putchar(text[i]);
+    for(i=0; text[i]; i++) {
+        text[i] = toupper((unsigned char)text[i]);
+        enc[i] = key[text[i]-'A'];
     }
+    enc[i] = '\0';
 
     // Decryption
-    printf("\nDecrypted: ");  
-    for(i = 0; text[i]; i++) {
-        if(isalpha(text[i])) {
-            char c = toupper(text[i]);
-            for(int j = 0; j < 26; j++)
-                if(key[j] == c) putchar('A' + j);
-        } else putchar(text[i]);
-    }
+    for(i=0; enc[i]; i++)
+        for(j=0; j<26; j++)
+            if(key[j] == enc[i])
+                dec[i] = 'A' + j;
+    dec[i] = '\0';
+    printf("Encrypted: %s\n", enc);
+    printf("Decrypted: %s\n", dec);
     return 0;
 }
