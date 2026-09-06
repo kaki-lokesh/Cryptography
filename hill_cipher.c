ae7b5c7bbd1c57e70163170d44b3f58abe24f56b
Lokesh Kaki <lkaki@student.gitam.edu>
2026-09-06 14:54:57 +0530

    Hill Substitution Cipher

--- hill_cipher.c
+++ hill_cipher.c
@@ -1,49 +1,46 @@
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
+#include <stdio.h>
 
-int isValid(char text[]) {
-    for(int i=0; text[i]!='\0'; i++) {
-        if(!isalpha((unsigned char)text[i]))
-            return 0;
-    }
-    return 1;
-}
-
-int main()
-{
-    int key[2][2];
-    char text[100];
+int main() {
+    int a[2][2];
+    int inv[2][2], det, i;
+    char text[100], enc[100], dec[100];
+    int x, y;
+    printf("Enter even-length text: ");
+    scanf("%s", text);
     printf("Enter 2x2 Key Matrix: \n");
     for(int i=0; i<2; i++)
         for(int j=0; j<2; j++)
-            scanf("%d", &key[i][j]);
-    printf("Enter Plain Text: ");
-    getchar();
-    fgets(text, sizeof(text), stdin);
-    text[strcspn(text, "\n")] = '\0';
-    if(!isValid(text)) {
-        printf("Invalid Input");
-        return 0;
+            scanf("%d", &a[i][j]);
+
+    // Encryption
+    for(i = 0; text[i]; i += 2) {
+        x = text[i] - 'A';
+        y = text[i+1] - 'A';
+        enc[i]   = (a[0][0]*x + a[0][1]*y) % 26 + 'A';
+        enc[i+1] = (a[1][0]*x + a[1][1]*y) % 26 + 'A';
     }
-    for(int i=0; text[i]; i++)
-        text[i] = toupper((unsigned char)text[i]);
-    int len = strlen(text);
-    if(len%2 != 0) {
-        text[len] = 'X';
-        text[len+1] = '\0';
-        len++;
+    enc[i] = '\0';
+
+    // Inverse matrix
+    det = (a[0][0]*a[1][1] - a[0][1]*a[1][0] + 26) % 26;
+    for(i = 1; i < 26; i++)
+        if(det * i % 26 == 1) break;
+    inv[0][0] = a[1][1] * i % 26;
+    inv[0][1] = -a[0][1] * i % 26;
+    inv[1][0] = -a[1][0] * i % 26;
+    inv[1][1] = a[0][0] * i % 26;
+    
+    // Decryption
+    for(i = 0; enc[i]; i += 2) {
+        x = enc[i] - 'A';
+        y = enc[i+1] - 'A';
+        dec[i]   = (inv[0][0]*x + inv[0][1]*y + 26) % 26 + 'A';
+        dec[i+1] = (inv[1][0]*x + inv[1][1]*y + 26) % 26 + 'A';
     }
-    printf("Cipher Text: ");
-    for(int i=0; i<len; i+=2) {
-        int a = text[i]-'A';
-        int b = text[i+1]-'A';
-        int c1 = (key[0][0]*a + key[0][1]*b)%26;
-        int c2 = (key[1][0]*a + key[1][1]*b)%26;
-        printf("%c%c", c1+'A', c2+'A');
-    }
+    dec[i] = '\0';
+    printf("Encrypted: %s\n", enc);
+    printf("Decrypted: %s\n", dec);
     return 0;
 }
-
 
 
